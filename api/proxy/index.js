84a8442f255c34a256bf3bb4b7dbb673100801ed
Ganes <ganes@selvi.com>
2025-10-12 19:08:49 +0530

    Fix Azure Function to extract path from x-ms-original-url header

--- api/proxy/index.js
+++ api/proxy/index.js
@@ -7,21 +7,47 @@ module.exports = async function (context, req) {
   context.log(`Query: ${JSON.stringify(context.req.query)}`);
   context.log(`Body: ${JSON.stringify(req.body)}`);
 
-  // Return the request details to debug
-  context.res = {
-    status: 200,
-    body: {
-      message: "Debug info",
-      originalUrl: context.req.url,
+  const backendBaseUrl = "http://52.172.26.253:5000";
+  
+  // Extract the path from the x-ms-original-url header
+  // The original URL is: https://ashy-bay-0f2f22600.1.azurestaticapps.net/api/auth/register
+  // We need to extract: /api/auth/register
+  let apiPath = "/";
+  if (req.headers && req.headers['x-ms-original-url']) {
+    const originalUrl = req.headers['x-ms-original-url'];
+    const url = new URL(originalUrl);
+    apiPath = url.pathname; // This will be /api/auth/register
+    context.log(`Extracted path from x-ms-original-url: ${apiPath}`);
+  }
+  
+  const backendUrl = `${backendBaseUrl}${apiPath}`;
+  context.log(`Proxying request to: ${backendUrl}`);
+
+  try {
+    const response = await fetch(backendUrl, {
       method: req.method,
-      query: context.req.query,
-      body: req.body,
-      headers: req.headers
-    },
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-      "Access-Control-Allow-Headers": "Content-Type, Authorization"
-    }
-  };
+      headers: { 
+        "Content-Type": "application/json",
+        ...req.headers
+      },
+      body: req.body ? JSON.stringify(req.body) : undefined
+    });
+
+    const data = await response.json();
+    context.res = {
+      status: response.status,
+      body: data,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization"
+      }
+    };
+  } catch (err) {
+    context.log(`Error: ${err.message}`);
+    context.res = {
+      status: 500,
+      body: { error: err.message }
+    };
+  }
 };
