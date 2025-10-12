80c6462c089390a93f5d60ebd49d5dafb598e045
Ganes <ganes@selvi.com>
2025-10-12 18:31:57 +0530

    Simplify Azure Function to test basic functionality

--- api/proxy/index.js
+++ api/proxy/index.js
@@ -5,44 +5,22 @@ module.exports = async function (context, req) {
   context.log(`Original URL: ${context.req.url}`);
   context.log(`Method: ${req.method}`);
   context.log(`Query: ${JSON.stringify(context.req.query)}`);
+  context.log(`Body: ${JSON.stringify(req.body)}`);
 
-  const backendBaseUrl = "http://52.172.26.253:5000";
-  
-  // For /api/ping, we need to extract /ping
-  // The URL comes as /api/ping, so we replace /api with empty string
-  let apiPath = context.req.url.replace("/api", "");
-  if (!apiPath) {
-    apiPath = "/";
-  }
-  
-  const backendUrl = `${backendBaseUrl}${apiPath}`;
-  context.log(`Proxying request to: ${backendUrl}`);
-
-  try {
-    const response = await fetch(backendUrl, {
+  // Simple test - just return the request info
+  context.res = {
+    status: 200,
+    body: {
+      message: "Function is working",
+      originalUrl: context.req.url,
       method: req.method,
-      headers: { 
-        "Content-Type": "application/json",
-        ...req.headers
-      },
-      body: req.body ? JSON.stringify(req.body) : undefined
-    });
-
-    const data = await response.json();
-    context.res = {
-      status: response.status,
-      body: data,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-        "Access-Control-Allow-Headers": "Content-Type, Authorization"
-      }
-    };
-  } catch (err) {
-    context.log(`Error: ${err.message}`);
-    context.res = {
-      status: 500,
-      body: { error: err.message }
-    };
-  }
+      query: context.req.query,
+      body: req.body
+    },
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization"
+    }
+  };
 };
