036b4dfb9e084340b2779ff5e599edd392389677
Ganes <ganes@selvi.com>
2025-10-12 19:02:52 +0530

    Add debug version to inspect request details

--- api/proxy/index.js
+++ api/proxy/index.js
@@ -7,56 +7,21 @@ module.exports = async function (context, req) {
   context.log(`Query: ${JSON.stringify(context.req.query)}`);
   context.log(`Body: ${JSON.stringify(req.body)}`);
 
-  const backendBaseUrl = "http://52.172.26.253:5000";
-  
-  // Extract the path from the query parameters
-  // For /api/auth/register, the path should be in req.query.restOfPath as "auth/register"
-  let apiPath = "/";
-  if (context.req.query && context.req.query.restOfPath) {
-    apiPath = `/${context.req.query.restOfPath}`;
-  } else {
-    // If restOfPath is not available, try to extract from the original URL
-    // The URL should be something like /api/proxy?restOfPath=auth/register
-    context.log('restOfPath not found in query, trying to extract from URL');
-    const urlParts = context.req.url.split('?');
-    if (urlParts.length > 1) {
-      const queryString = urlParts[1];
-      const params = new URLSearchParams(queryString);
-      const restOfPath = params.get('restOfPath');
-      if (restOfPath) {
-        apiPath = `/${restOfPath}`;
-      }
-    }
-  }
-  
-  const backendUrl = `${backendBaseUrl}${apiPath}`;
-  context.log(`Proxying request to: ${backendUrl}`);
-
-  try {
-    const response = await fetch(backendUrl, {
+  // Return the request details to debug
+  context.res = {
+    status: 200,
+    body: {
+      message: "Debug info",
+      originalUrl: context.req.url,
       method: req.method,
-      headers: { 
-        "Content-Type": "application/json",
-        ...req.headers
-      },
-      body: req.body ? JSON.stringify(req.body) : undefined
-    });
-
-    const data = await response.json();
-    context.res = {
-      status: response.status,
-      body: data,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-        "Access-Control-Allow-Headers": "Content-Type, Authorization"
-      }
-    };
-  } catch (err) {
-    context.log(`Error: ${err.message}`);
-    context.res = {
-      status: 500,
-      body: { error: err.message }
-    };
-  }
+      query: context.req.query,
+      body: req.body,
+      headers: req.headers
+    },
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization"
+    }
+  };
 };
